a6ce5f89e809a0e884240fa026d9326fefc0c1ed
tuvieja <105611148+n1na666@users.noreply.github.com>
2023-08-24 12:44:51 -0500

    arreglando el mongoose

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,9 @@ app.get("/", (req,res)=>{
 
 
 // Connect to DB
-
 const PORT = process.env.PORT || 5000;
+
+mongoose.set('strictQuery', true);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
